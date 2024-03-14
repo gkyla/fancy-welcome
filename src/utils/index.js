4b15adecc2f81d1bd7f45b6f3e81b0a007652d4a
gkyla <agitsyang@gmail.com>
2024-03-14 23:28:20 +0700

    change utils functon

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+import ms from "ms";
+
 export function toMS(s) {
-  return parseFloat(s) * (/\ds$/.test(s) ? 1000 : 1);
+  // return parseFloat(s) * (/\ds$/.test(s) ? 1000 : 1);
+  return ms(s);
 }
